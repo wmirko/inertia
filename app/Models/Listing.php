2589590e38fc2eac490e949dcd3c8239b9979363
wmirko <mirko.wordell@gmx.de>
2022-10-24 11:42:42 +0000

    rename/save old Listing, replace with artisan make:modell

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,51 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    protected int $id;
-    protected string $label = '';
-    protected string $description = '';
-
-    public static function getAll():array {
-        $res = [
-            (new self())
-            ->setId(random_int(1,100))
-            ->setLabel(bin2hex(random_bytes(10)))
-            ->setDescription(bin2hex(random_bytes(30))),
-            (new self())
-            ->setId(random_int(1,100))
-            ->setLabel(bin2hex(random_bytes(10)))
-            ->setDescription(bin2hex(random_bytes(30)))
-        ];
-
-        return $res;
-    }
-
-    public function setId(int $id){
-        $this->id = $id;
-        return $this;
-    }
-
-    public function setLabel(string $label){
-        $this->label = $label;
-        return $this;
-    }
-    public function setDescription(string $description){
-        $this->description = $description;
-        return $this;
-    }
-
-    public function getId():int {
-       return $this->id;
-    }
-
-    public function getLabel():string {
-        return $this->label;    
-    }
-
-    public function getDescription():string {
-        return$this->description;
-    }
-
+class Listing extends Model
+{
+    use HasFactory;
 }
