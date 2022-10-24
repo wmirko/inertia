560395931e9a4690423992a9ca162cae4f5e74f1
wmirko <mirko.wordell@gmx.de>
2022-10-24 11:43:38 +0000

    add new migration: listings

---
+++ database/migrations/2022_10_24_080732_create_listings_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings_tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('tags');
+            $table->string('title');
+            $table->string('company');
+            $table->string('location');
+            $table->string('website');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings_tables');
+    }
+};
