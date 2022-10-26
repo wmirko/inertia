a6f6f6ba69054b954cf8dbe5a7b09968c4b50331
wmirko <mirko.wordell@gmx.de>
2022-10-26 18:48:04 +0000

    add controller methods index, show

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\LaravelIgnition\Recorders\DumpRecorder\Dump;
+use App\Models\Listing;
 
 class ListingController extends Controller
 {
-    //
+    // get single object
+    public function show(Listing $listing) 
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    
+    // show all
+    public function index()
+    {
+        return view('listings.index', [
+            'heading' => 'topics from the presnet',
+            'listings' => Listing::all()
+        ]);
+    }
+
+
 }
