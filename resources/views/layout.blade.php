3d12e07c22503e1b2f02b94eb3c6261bf2652376
wmirko <mirko.wordell@gmx.de>
2022-10-26 10:41:34 +0000

    add basic layout view file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" href="images/favicon.ico" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="//unpkg.com/alpinejs" defer></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              laravel: '#ef3b2d',
+            },
+          },
+        },
+      }
+  </script>
+  <title>LaraGigs | Find Laravel Jobs & Projects</title>
+</head>
+<body>
+   @include('navbar')
+
+   <div style="display: flex; justify-content: space-between; flex-direction: row; flex-flow: column wrap; width:80%;">
+    @yield('listings')
+
+   </div>
+</body>
+</html>
