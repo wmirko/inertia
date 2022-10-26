e5bba40c7f9660f190c5b13b61e14101b19cd04a
wmirko <mirko.wordell@gmx.de>
2022-10-26 10:25:41 +0000

    rework view - tailwind style

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,18 +1,22 @@
+@extends('layout')
 
-<h1>{{$heading}}</h1>
+@section('listings')
+@include('partials._hero')
+@include('partials._search')
 
 @foreach( $listings as $listing)
-
-<div>
-<label for="label-{{$listing->getId()}}"> 
-    Label:
-    <input type="text" disabled id="label-{{$listing->getId()}}" value="{{$listing->getLabel()}}">
-</label>
-
-<label for="desc-{{$listing->getId()}}" >
-    Description:
-    <input type="text" disabled id="desc-{{$listing->getId()}}" value="{{$listing->getDescription()}}">
-</label>
+<div class="flex justify-center" style="max-width: 120mm; max-height: 70mm; margin: 20mm;">
+  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+    <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt="" />
+    <div class="p-6 flex flex-col justify-start">
+      <h5 class="text-gray-900 text-xl font-medium mb-2"><a href="{{$listing->getKey()}}">{{$listing['title']}}</a></h5>
+      <p class="text-gray-700 text-base mb-4">
+      {{$listing->description}}
+      </p>
+      <p class="text-gray-600 text-xs">{{$listing->timestamps}}</p>
+    </div>
+  </div>
 </div>
 
-@endforeach;
+@endforeach
+@endsection
