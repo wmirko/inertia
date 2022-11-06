e371e9f207e9e980870a9ca53ca97b1e5a60cd72
wmirko <mirko.wordell@gmx.de>
2022-11-06 20:37:25 +0000

    code style

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
-@foreach( $listings as $listing)
-<div class="flex" style="max-height: 70mm; margin: 10mm;">
-  <x-listing :listing="$listing" :url="'https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg'"></x-listing>
-</div>
-@endforeach
+  @foreach( $listings as $listing)
+  <div class="flex" style="max-height: 70mm; margin: 10mm;">
+    <x-listing :listing="$listing" :url="'https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg'"></x-listing>
+  </div>
+  @endforeach
 </x-layout>
